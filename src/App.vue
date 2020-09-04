717c3cf671478832cb84d5435e46fd55eaf6f67e
SKY-20170606MFZ\Administrator <987589735@qq.com>
2020-09-04 11:22:20 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <tab-bar></tab-bar>
+  </div>
+</template>
+
+<script>
+import TabBar from 'components/tabbar/TabBar'
+
+export default {
+  name: "App",
+  components: {
+    TabBar
+  },
+};
+</script>
+
+<style>
+@import url('~style/base.css');
+
+</style>
